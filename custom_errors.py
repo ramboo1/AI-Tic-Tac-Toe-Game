7fd7dd7f60eca82c82b7312d55b367f19316c8fe
Rishit Sidhwa <64678294+ramboo1@users.noreply.github.com>
2023-09-01 16:48:34 +0530

    Add files via upload

---
+++ custom_errors.py
@@ -0,0 +1,14 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class InvalidActionError(Error):
+    """Exception raised for errors in the input.
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, action, board, message):
+        print('InvalidActionError: ', message, 'Action: ', action, 'on board: ', board)
